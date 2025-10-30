30e6878a6c82e498c9ee1f2d55aa05b70dae7917
Paul Ruddies <anjaruddies1@gmail.com>
2025-10-30 12:14:17 +0100

    Test 1

--- Projekt/Program.cs
+++ Projekt/Program.cs
@@ -11,6 +11,7 @@ namespace Projekt
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            Console.WriteLine("Hallo Test");
         }
     }
 }
