5a3325de7561f293e7250d78b4ed1a7f67344148
David Callea <privatdavid123@gmail.com>
2025-12-18 12:02:20 +0100

    Dunguen ausgeben

--- Projekt/Program.cs
+++ Projekt/Program.cs
@@ -10,9 +10,54 @@ namespace Projekt
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
-            Console.WriteLine("Hallo Test");
-            Console.WriteLine("moinsen");
+            int höhe;
+            int breite;
+
+            Console.WriteLine("Willkommen, bitte breite und länge eingeben: ");
+
+
+
+            string eingabe = Console.ReadLine();
+            while (!int.TryParse(eingabe, out höhe))
+            {
+                Console.Write("Erneut eingeben: ");
+                eingabe = Console.ReadLine();
+            }
+            string eingabe2 = Console.ReadLine();
+            while (!int.TryParse(eingabe2, out breite))
+            {
+                Console.Write("Erneut eingeben: ");
+                eingabe2 = Console.ReadLine();
+            }
+
+            char[,] dunguen = new char[höhe, breite];
+
+            FüllenMitWänden(dunguen);
+
+            DungeonAusgeben(dunguen);
+            Console.ReadLine();
+        }
+
+        // Wände Füllen mit #
+        static void FüllenMitWänden(char[,] karte)
+        {
+            for (int y = 0; y < karte.GetLength(0); y++)
+                for (int x = 0; x < karte.GetLength(1); x++)
+                    karte[y, x] = '#';
+        }
+
+        // Dunguen ausgeben
+        static void DungeonAusgeben(char[,] karte)
+        {
+            for (int y = 0; y < karte.GetLength(0); y++)
+            {
+                for (int x = 0; x < karte.GetLength(1); x++)
+                {
+                    char feld = karte[y, x];
+                    Console.Write(feld);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
